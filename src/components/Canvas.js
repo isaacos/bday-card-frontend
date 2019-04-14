32ccf07b77d88275a373cddea596a36bcfbffc01
isaac <oseasisaac91@gmail.com>
2019-04-14 15:37:51 -0400

    adds ability to draw squares and bigLines, also modifies the drawLine function to take in the argument that determines if the most recently drawn line should be saved into the previousLines in state

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -15,27 +15,46 @@ class Canvas extends Component {
     previousLines: [],
     undoneLines: [],
     undraw: true,
-    redraw: true
+    redraw: true,
+    setting: 'paint'
   }
 
   mouseMoveHelper = (event) => {
-    this.setState({startX: event.clientX, startY: event.clientY})
-    if(this.state.mouseDown){
-    this.drawLine(event)
+    if(this.state.setting !== 'bigLine'){
+        this.setState({startX: event.clientX, startY: event.clientY})
+        if(this.state.mouseDown){
+            this.drawLine(event)
+        }
+    }
+    //draws bigLine on page after mouseDown
+    if(this.state.setting === 'bigLine' && this.state.mouseDown){
+      //refreshes page but does not commit line to previous Lines, to prevents lines appearing with every motion
+      this.refresh()
+      this.redrawAll(this.state.previousLines)
+      this.drawLine(event, 0, 'dont commit')
     }
   }
 
+
+
   onMouseDownHelper = (event) => {
-    this.setState({mouseDown: true})
-    this.drawLine(event, 1)
+    if(this.state.setting === 'bigLine'){
+      this.setState({startX: event.clientX, startY: event.clientY, mouseDown: true})
+    } else {
+      this.setState({mouseDown: true})
+      this.drawLine(event, 1)
+    }
   }
 
-  drawLine = (event, minusClientX =0) => {
+  drawLine = (event, minusClientX =0, commit = 'commit') => {
     const offsetLeft = this.refs.canvas.offsetLeft
-    let storedLine = {startX: this.state.startX - offsetLeft, startY: this.state.startY, clientX:  event.clientX - minusClientX - offsetLeft, clientY: event.clientY, color: this.state.color, radius: this.state.radius}
+    let storedLine = {startX: this.state.startX - offsetLeft, startY: this.state.startY, clientX:  event.clientX - minusClientX - offsetLeft, clientY: event.clientY, color: this.state.color, radius: this.state.radius, setting: this.state.setting}
     this.drawHelper(storedLine)
-    let newLines =  [...this.state.previousLines, storedLine]
-    this.setState({previousLines: newLines})
+    //commits new lines to previousLines except for mouseMove helper for bigLines
+    if(commit === 'commit'){
+      let newLines =  [...this.state.previousLines, storedLine]
+      this.setState({previousLines: newLines})
+    }
   }
 
   redrawAll = (prevLines) => {
@@ -47,14 +66,44 @@ class Canvas extends Component {
   drawHelper = (lineObj) => {
     const canvas = this.refs.canvas
     const ctx = canvas.getContext('2d')
-    ctx.lineJoin = 'round' //turn to line
-    ctx.lineWidth = lineObj['radius']
-    ctx.beginPath();
+    if(lineObj['setting'] === 'paint' || lineObj['setting'] === 'bigLine'){
+      this.paintSettingDraw(ctx, lineObj)
+    }
+    if(lineObj['setting'] === 'square'){
+      this.squareSettingDraw(ctx, lineObj)
+    }
+  }
+
+//determines lineJoin based on the paint setting
+  setLineJoin = (setting) => {
+    switch(setting){
+      case 'paint':
+        return 'round'
+      case 'bigLine':
+        return 'miter'
+      case 'square':
+        return 'bevel'
+    }
+  }
+
+  paintSettingDraw = (ctx, lineObj) => {
     ctx.strokeStyle = lineObj['color'];
+    ctx.beginPath();
+    ctx.lineWidth = lineObj['radius']
+    ctx.lineJoin = this.setLineJoin(lineObj['setting'])
     ctx.moveTo(lineObj['startX'], lineObj['startY'])
     ctx.lineTo(lineObj['clientX'], lineObj['clientY'])
     ctx.closePath()
     ctx.stroke();
+  }
+
+  squareSettingDraw = (ctx, lineObj) => {
+    ctx.lineJoin = this.setLineJoin(lineObj['setting'])
+    ctx.fillStyle = lineObj['color']
+    //set cursor in the middle fo the square
+    const startX =lineObj['startX'] - (lineObj['radius'] / 2)
+    const startY = lineObj['startY'] - (lineObj['radius'] / 2)
+    ctx.fillRect(startX, startY, lineObj['radius'], lineObj['radius'])
   }
 
 //clears canvas completely
@@ -75,14 +124,20 @@ class Canvas extends Component {
       case 'large':
         return this.setState({radius: 27});
       case 'chunka':
-        return this.setState({radius: 60})
+        return this.setState({radius: 100})
     }
   }
 
-  mouseDownFalse = () => {
+  mouseUpOrOutHelper = (event) => {
     if(this.state.mouseDown){
       let newPreviousLines = [...this.state.previousLines, this.state.newestLines]
       this.setState({mouseDown: false, previousLines: newPreviousLines})
+      //commits the bigLine onMouseUp 
+      if(this.state.setting === 'bigLine'){
+        this.refresh()
+         this.redrawAll(this.state.previousLines)
+         this.drawLine(event, 0)
+      }
     }
   }
 
@@ -155,7 +210,6 @@ class Canvas extends Component {
 
 
   render () {
-  console.log(this.state.undoneLines)
     return (
       <div>
         <canvas
@@ -164,9 +218,9 @@ class Canvas extends Component {
          width={900}
          height={424}
          onMouseDown={e=>this.onMouseDownHelper(e)}
-         onMouseUp={()=>this.mouseDownFalse()}
+         onMouseUp={e =>this.mouseUpOrOutHelper(e)}
          onMouseMove={e => this.mouseMoveHelper(e)}
-         onMouseOut={() => this.mouseDownFalse()}
+         onMouseOut={e => this.mouseUpOrOutHelper(e)}
         /><br />
         <CompactPicker
           color={this.state.color}
@@ -178,7 +232,10 @@ class Canvas extends Component {
         <button onClick={() => this.brushChange('large')}>Large</button>
         <button onClick={() => this.brushChange('chunka')}>Chunka</button>
         <button onClick={() => this.downloadImage()}>download image</button>
-        <button onClick={() => this.refresh()}>refresh</button>
+        <button onClick={() => this.setState({setting: 'paint'})}>paint</button>
+        <button onClick={() => this.setState({setting: 'square'})}>Square</button>
+        <button onClick={() => this.setState({setting: 'bigLine'})}>Big Line</button>
+        <button onClick={() => this.refresh()} onMouseDown={() => this.setState({previousLines: [], undoneLines: []})}>refresh</button>
         <button onMouseDown={() => this.redraw()} onMouseUp={() => this.setState({redraw: false})}>ReDraw</button>
         <button onMouseDown={() => this.undraw()} onMouseUp={() => this.setState({undraw: false})}>undraw</button>
         <input type="email" placeholder="email" onChange={event => this.setState({email: event.target.value})} />
